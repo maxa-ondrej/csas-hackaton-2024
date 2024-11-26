13d35a0a6eb4d829a831cc9829c5490cc0ee030d
Ondřej Maxa <omaxa@csas.cz>
2024-11-26 20:37:48 +0100

    fix: fix

--- src/chat/status-indicator.tsx
+++ src/chat/status-indicator.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function ChatStatusIndicator({ status }: Props) {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-row items-center text-pink-300">
+      <div className="flex flex-row items-center text-pink-500">
         <div className="m-2 animate-spin">
           <SiOpenai />
         </div>
