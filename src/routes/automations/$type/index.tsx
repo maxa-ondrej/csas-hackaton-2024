29bd8394ac5f9ff54e29aa7b0279d6a9a295614f
Michal Janočko <33790048+michaljanocko@users.noreply.github.com>
2024-11-26 12:24:14 +0100

    feat: view logs for jobs

--- src/routes/automations/$type/index.tsx
+++ src/routes/automations/$type/index.tsx
@@ -1,8 +1,17 @@
-import { type Automation, getAutomations } from '@/lib/client';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import type { Automation } from '@/lib/client';
 import { getAutomationsOptions } from '@/lib/client/@tanstack/react-query.gen';
 import { createLoader } from '@/lib/loader';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import type { ColumnDef } from '@tanstack/react-table';
+import { formatDistanceToNow } from 'date-fns';
 
 export const Route = createFileRoute('/automations/$type/')({
   component: RouteComponent,
@@ -24,5 +33,43 @@ const columns: ColumnDef<Automation>[] = [
 ];
 
 function RouteComponent() {
-  return;
+  const { automations } = Route.useLoaderData();
+  const { type } = Route.useParams();
+  const navigate = useNavigate();
+
+  return (
+    <section className="p-4 md:p-8 w-full overflow-auto">
+      <h1 className="text-2xl mb-6">{type}</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Last activity</TableHead>
+            <TableHead>SAS</TableHead>
+            <TableHead>State</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {automations.map((automation) => (
+            <TableRow
+              key={automation.id}
+              onClick={() =>
+                navigate({ to: `/automations/${type}/${automation.id}/logs` })
+              }
+              className="cursor-pointer"
+            >
+              <TableCell>{automation.id}</TableCell>
+              <TableCell>
+                {formatDistanceToNow(Date.parse(automation.last_activity), {
+                  addSuffix: true,
+                })}
+              </TableCell>
+              <TableCell>{automation.sas}</TableCell>
+              <TableCell>{automation.state}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  );
 }
